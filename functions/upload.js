64c78047d09687b0753d5a2c1b2312cdba9dbc75
khand-web <xm.khand@gmail.com>
2026-06-05 13:34:40 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,3 +1,22 @@
-export async function onRequest() {
-  return new Response("FUNCTION IS REACHING CLOUDFLARE");
+export async function onRequestPost({ request }) {
+  const form = await request.formData();
+
+  const name = form.get("name");
+  const slug = form.get("slug");
+  const file = form.get("file");
+
+  if (!name || !slug || !file) {
+    return new Response("Missing fields");
+  }
+
+  return new Response(
+    JSON.stringify({
+      ok: true,
+      message: "UPLOAD SUCCESS",
+      name,
+      slug,
+      fileName: file.name
+    }),
+    { headers: { "Content-Type": "application/json" } }
+  );
 }
