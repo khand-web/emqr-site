fadbdaa4890b995c8f353186c73399f2d32da9c5
khand-web <xm.khand@gmail.com>
2026-06-05 13:31:15 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,24 +1,3 @@
-export async function onRequest({ request }) {
-  const method = request.method;
-
-  if (method !== "POST") {
-    return new Response("API READY (POST required)");
-  }
-
-  const form = await request.formData();
-
-  const name = form.get("name");
-  const slug = form.get("slug");
-
-  return new Response(
-    JSON.stringify({
-      ok: true,
-      message: "UPLOAD WORKS",
-      name,
-      slug
-    }),
-    {
-      headers: { "Content-Type": "application/json" }
-    }
-  );
+export async function onRequest() {
+  return new Response("FUNCTION IS REACHING CLOUDFLARE");
 }
