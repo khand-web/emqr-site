5767966ed1139044e767596c9655f0635fe14bb5
khand-web <xm.khand@gmail.com>
2026-06-05 12:33:38 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,30 +1,3 @@
-export async function onRequestPost({ request, env }) {
-  const form = await request.formData();
-
-  const file = form.get("file");
-  const name = form.get("name");
-  const slug = form.get("slug");
-
-  if (!file || !slug) {
-    return new Response("Missing data", { status: 400 });
-  }
-
-  // PDF-г R2-д хадгална
-  await env.MED_BUCKET.put(
-    em/${slug}.pdf,
-    file.stream(),
-    {
-      httpMetadata: {
-        contentType: "application/pdf"
-      }
-    }
-  );
-
-  return new Response(
-    JSON.stringify({
-      ok: true,
-      url: `https://emqr.mn/em/${slug}.pdf`
-    }),
-    { headers: { "Content-Type": "application/json" } }
-  );
+export async function onRequestPost() {
+  return new Response("UPLOAD API WORKING");
 }
